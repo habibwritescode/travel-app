988b9e873340c46b0351f9b3730e7dfcde2c1601
Habib Afolabi <habibwritescode@gmail.com>
2020-04-20 14:25:22 +0100

    Add basic server code

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Travel App
 
 ## Overview
+This project includes a simple form where you enter the location you are traveling to and the date you are leaving. If the trip is within a week, you will get the current weather forecast. If the trip is in the future, you will get a predicted forecast.
 
 
 ## Functionality
--- src/server/server.js
+++ src/server/server.js
@@ -1,9 +1,14 @@
 // Setup empty JS object to act as endpoint for all routes
-let projectData = {};
+const projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
+
+/*Dependencies*/
+const bodyParser = require('body-parser');
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,9 +16,19 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
-app.use(express.static('website'));
+app.use(express.static('./src/client'));
 
 
 // Setup Server
+const port = 3000;
+
+const server = app.listen(port, listening);
+// Callback to debug
+function listening() {
+    console.log('server is running');
+    console.log(`running on localhost: ${port}`);
+}
