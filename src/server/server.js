645194e99797f275decc1d9039c5519e5ca3e1e6
Habib Afolabi <habibwritescode@gmail.com>
2020-12-19 12:49:48 +0100

    Change port for heroku

--- src/server/server.js
+++ src/server/server.js
@@ -26,7 +26,7 @@ app.get('/', function(req, res) {
 })
 
 // Setup Server
-const port = 3000;
+const port = process.env.PORT || 3000;
 const server = app.listen(port, listening);
 // Callback to debug
 function listening() {
